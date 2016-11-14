13032d9c327b73ab6f87025853de21a64091c8ec
pedronovaes <pedronovaesmelo@gmail.com>
2016-11-14 19:09:57 -0300

    Adding jpneu.cpp code

---
+++ JPNEU/jpneu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int N, M;
+
+    scanf(" %d", &N);
+    scanf(" %d", &M);
+
+    printf("%d\n", N - M);
+    return 0;
+}
