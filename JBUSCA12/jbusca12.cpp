a8b737ace40998e3fda5fd4bb403c403e95dfe59
pedronovaes <pedronovaesmelo@gmail.com>
2016-11-14 19:27:39 -0300

    Adding jbusca12.cpp code

---
+++ JBUSCA12/jbusca12.cpp
@@ -0,0 +1,14 @@
+// Pedro Marcelino Mendes Novaes Melo
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int t;
+
+    scanf(" %d", &t);
+
+    printf("%d\n", t*4);
+    return 0;
+}
