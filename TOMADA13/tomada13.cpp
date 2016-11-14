0b73f97f831d5ebbfa9660300bf24e92758b17af
pedronovaes <pedronovaesmelo@gmail.com>
2016-11-14 19:34:02 -0300

    Adding tomada13.cpp code

---
+++ TOMADA13/tomada13.cpp
@@ -0,0 +1,14 @@
+// Pedro Marcelino Mendes Novaes Melo
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int t1, t2, t3, t4;
+
+    scanf(" %d %d %d %d", &t1, &t2, &t3, &t4);
+
+    printf("%d\n", (t1 + t2 + t3 + t4) - 3);
+    return 0;
+}
