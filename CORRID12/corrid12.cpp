947c6ec8775dd772a9b8dd1e0c1e5c4c3ea91903
pedronovaes <pedronovaesmelo@gmail.com>
2016-11-14 19:18:01 -0300

    Adding corrid12.cpp code

---
+++ CORRID12/corrid12.cpp
@@ -0,0 +1,14 @@
+// Pedro Marcelino Mendes Novaes Melo
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int C, N;
+
+    scanf(" %d %d", &C, &N);
+
+    printf("%d\n", C%N);
+    return 0;
+}
